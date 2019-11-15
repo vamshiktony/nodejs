4c4cdb9b07935f298e2808f63e6db0141632078d
vamshiktony <51155045+vamshiktony@users.noreply.github.com>
2019-11-15 15:17:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(3000, "172.31.18.228");
-console.log('Server running at http://172.31.18.228:3000/');
+}).listen(3000, "172.31.9.93");
+console.log('Server running at http://172.31.9.93:3000/');
